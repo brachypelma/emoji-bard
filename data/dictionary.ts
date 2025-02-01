6c51c2c7c0e642d0bc44dd7e5ef5b3512634db90
Eric Jonas <ericdjonas@gmail.com>
2025-02-01 13:11:02 -0500

    Updated dictionary

--- data/dictionary.ts
+++ data/dictionary.ts
@@ -36,5 +36,204 @@ export const dictionary: DictionaryEntry[] = [
     emoji: '😀',
     words: ['happy', 'glad', 'smile', 'grin', 'beam', 'beaming'],
   },
-  
+  {
+    emoji: '🍬',
+    words: ['sweets', 'candy', 'sweet'],
+  },
+  {
+    emoji: '❤️',
+    words: ['love', 'heart', 'valentine', 'valentines'],
+  },
+  {
+    emoji: '🍬❤️',
+    words: ['sweetheart'],
+  },
+  {
+    emoji: '👁️',
+    words: ['eye', 'I'],
+  },
+  {
+    emoji: '👀',
+    words: ['eyes', 'look', 'stare', 'glance', 'watch'],
+  },
+  {
+    emoji: '⏰',
+    words: ['clock', 'time', 'alarm clock', 'when', 'ETA'],
+  },
+  {
+    emoji: '💊😱🫖🫖',
+    words: ['pilates'],
+  },
+  {
+    emoji: '🔥',
+    words: ['fire', 'flames', 'hot', 'flame', 'burn', 'burning'],
+  },
+  {
+    emoji: '🔥💊😱🫖🫖',
+    words: ['hot pilates'],
+  },
+  {
+    emoji: '🪟',
+    words: ['window', 'sill', 'silly'],
+  },
+  {
+    emoji: '🪿',
+    words: ['goose', 'gander'],
+  },
+  {
+    emoji: '➰',
+    words: ['curl', 'curling', 'curls', 'q', 'queue'],
+  },
+  {
+    emoji: '➰🫖',
+    words: ['cutie'],
+  },
+  {
+    emoji: '🥧',
+    words: ['pie', 'pastry'],
+  },
+  {
+    emoji: '⛽',
+    words: ['pump'],
+  },
+  {
+    emoji: '👪',
+    words: ['family', 'kin', 'relations', 'relatives'],
+  },
+  {
+    emoji: '⛽👪',
+    words: ['pumpkin'],
+  },
+  {
+    emoji: '🎃',
+    words: ['pumpkin', 'jack-o-lantern', 'halloween', 'Jack', 'lantern', 'squash', 'gourd'],
+  },
+  {
+    emoji: '🪼',
+    words: ['jelly', 'jam', 'jellyfish'],
+  },
+  {
+    emoji: '📧',
+    words: ['email'],
+  },
+  {
+    emoji: '🎤',
+    words: ['microphpone', 'mike', 'Mike', 'mic'],
+  },
+  {
+    emoji: '📧🎤',
+    words: ['email Mike'],
+  },
+  {
+    emoji: '🐝',
+    words: ['bee', 'be'],
+  },
+  {
+    emoji: '🥩',
+    words: ['steak', 'meat', 'meet', 'beef', 'chop', 'tenderloin', 'loin'],
+  },
+  {
+    emoji: '⚽',
+    words: ['ball', 'soccer', 'football', 'footie', 'soccer ball'],
+  },
+  {
+    emoji: '🎶',
+    words: ['music', 'note', 'notes', 'song', 'melody', 'beat', 'beet', 'diddy'],
+  },
+  {
+    emoji: '🏆',
+    words: ['trophy', 'cup', 'champion', 'championship', 'winner', 'award'],
+  },
+  {
+    emoji: '🥩⚽',
+    words: ['meatball'],
+  },
+  {
+    emoji: '🎶🏆',
+    words: ['Tony', 'Grammy'],
+  },
+  {
+    emoji: '🌱',
+    words: ['plant', 'sprout', 'seedling', 'vegan', 'plant-based', 'vegetarian'],
+  },
+  {
+    emoji: '🤠',
+    words: ['cowboy', 'cowthey', 'ranch', 'rancher', 'yeehaw', 'ranger'],
+  },
+  {
+    emoji: '⏰➡️',
+    words: ['after', 'future', 'forward', 'ahead'],
+  },
+  {
+    emoji: '⬅️⏰',
+    words: ['before', 'past', 'backward', 'behind'],
+  },
+  {
+    emoji: '⏰↔️',
+    words: ['whenever', 'timeline'],
+  },
+  {
+    emoji: '⬆️',
+    words: ['up', 'big', 'large', 'high', 'tall', 'upward', 'upwards'],
+  },
+  {
+    emoji: '🏋️',
+    words: ['weight', 'wait', 'pump', 'lift', 'lifting', 'weightlifting', 'weightlifter'],
+  },
+  {
+    emoji: '🐄',
+    words: ['cow', 'steer', 'dairy', 'bovine'],
+  },
+  {
+    emoji: '🐄🌱',
+    words: ['cud', 'cuddle'],
+  },
+  {
+    emoji: '🍑',
+    words: ['peach'],
+  },
+  {
+    emoji: '⚪',
+    words: ['cue', 'cueball', 'cue ball', 'q', 'queue'],
+  },
+  {
+    emoji: '⚪🫖',
+    words: ['cutie'],
+  },
+  {
+    emoji: '🌙',
+    words: ['moon', 'night', 'nighttime', 'moonlight'],
+  },
+  {
+    emoji: '🌙🎶',
+    words: ['lullaby'],
+  },
+  {
+    emoji: '🏊‍♀️',
+    words: ['dip', 'swim', 'swimming', 'swimmer'],
+  },
+  {
+    emoji: '😘',
+    words: ['kiss', 'kissing'],
+  },
+  {
+    emoji: '🧠',
+    words: ['brain', 'cerebrum', 'cerebral', 'cerebral cortex', 'think', 'thinking', 'mind', 'ponder', 'thought', 'pondering'],
+  },
+  {
+    emoji: '⬆️🧠',
+    words: ['smart', 'intelligent'],
+  },
+  {
+    emoji: '🥛',
+    words: ['milk', 'dairy', 'cream'],
+  },
+  {
+    emoji: '🤝',
+    words: ['shake', 'handshake', 'agree', 'agreement', 'deal'],
+  },
+  {
+    emoji: '🥛🤝',
+    words: ['milkshake'],
+  },
 ]
